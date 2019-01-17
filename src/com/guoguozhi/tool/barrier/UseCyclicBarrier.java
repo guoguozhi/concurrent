c545633747e840448bd356a33b9e701aa36bd31f
guoguozhi <1165325898@qq.com>
2019-01-17 17:30:21 +0800

    测试CyclicBarrier的带线程的构造函数，发现在多线程编程中Thread.currentThread().getName()可能有问题

--- src/com/guoguozhi/tool/barrier/UseCyclicBarrier.java
+++ src/com/guoguozhi/tool/barrier/UseCyclicBarrier.java
@@ -6,23 +6,25 @@ import java.util.concurrent.CyclicBarrier;
 public class UseCyclicBarrier {
 
     // 循环屏障
-    private static CyclicBarrier barrier = new CyclicBarrier(3);
+    //private static CyclicBarrier barrier = new CyclicBarrier(3);
+    private static CyclicBarrier barrier = new CyclicBarrier(3, new BarrierAction());
 
     // 工作线程
     private static class WorkingThread extends Thread {
         @Override
         public void run() {
             if (!isInterrupted()) {
-                System.out.println(Thread.currentThread().getName() + " is ready to do it's work.");
+                String threadName = Thread.currentThread().getName();
+                System.out.println(threadName + " is ready to do it's work.");
                 for (int i = 0; i < 2; i++) {
-                    System.out.println(Thread.currentThread().getName() + " is doing it's step one work.");
+                    System.out.println(threadName + " is doing it's step one work.");
                 }
 
-                System.out.println(Thread.currentThread().getName() + "  完成了第一步");
+                System.out.println(threadName + "  完成了第一步");
 
                 try {
                     barrier.await();  // 阻塞方法，线程会卡到此处直至等待其他线程都到齐了，后面的代码才会执行
-                    System.out.println(Thread.currentThread().getName() + " 等待结束");
+                    System.out.println(threadName+ " 等待结束 the number of waiting thread is  " +  barrier.getNumberWaiting());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (BrokenBarrierException e) {
@@ -30,10 +32,23 @@ public class UseCyclicBarrier {
                 }
 
                 for (int i = 0; i < 2; i++) {
-                    System.out.println(Thread.currentThread().getName() + " is doing it's step two work.");
+                    System.out.println(threadName + " is doing it's step two work.");
                 }
 
-                System.out.println(Thread.currentThread().getName() + " has finished it's work.");
+                System.out.println(threadName + " has finished it's work.");
+            }
+        }
+    }
+
+    // 屏障开启后执行的线程
+    private static class BarrierAction extends Thread {
+        @Override
+        public void run() {
+            if (!isInterrupted()) {
+                Thread.currentThread().setName("barrierAction");
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + " do something. the number of waiting thread is  " +  barrier.getNumberWaiting());
+                }
             }
         }
     }
