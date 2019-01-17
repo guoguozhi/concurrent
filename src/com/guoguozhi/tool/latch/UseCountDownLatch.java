bd5f9df90db4bf22e3a6644fdfc1fd568f6bec47
guoguozhi <1165325898@qq.com>
2019-01-17 11:57:04 +0800

    继续CountDownLatch的理解

--- src/com/guoguozhi/tool/latch/UseCountDownLatch.java
+++ src/com/guoguozhi/tool/latch/UseCountDownLatch.java
@@ -18,13 +18,13 @@ public class UseCountDownLatch {
         public void run() {
             if (!isInterrupted()) {
                 Thread.currentThread().setName("InitThread");
-                System.out.println(Thread.currentThread().getName() + " is ready to do some initial work.");
+                System.out.println(Thread.currentThread().getName() + " is ready to do some initial work. latch=" + latch.getCount());
                 latch.countDown(); // 只会将计数器-1
                 System.out.println(Thread.currentThread().getName() + " latch 的计数器值: " + latch.getCount());
                 for (int i = 0; i < 2; i++) {
-                    System.out.println(Thread.currentThread().getName() + " continue does it's work.");
+                    System.out.println(Thread.currentThread().getName() + " continue does it's work. latch=" + latch.getCount());
                 }
-                System.out.println(Thread.currentThread().getName() + " has finished it's initial work.");
+                System.out.println(Thread.currentThread().getName() + " has finished it's initial work. latch=" + latch.getCount());
                 // 只有放在最后面才能保证创建的6个线程都执行结束以后，main线程后面的代码才会被调度，有时需要特别注意代码的位置
                 //latch.countDown();
             }
@@ -37,14 +37,17 @@ public class UseCountDownLatch {
         public void run() {
             if (!isInterrupted()) {
                 Thread.currentThread().setName("businessThread");
-                System.out.println(Thread.currentThread().getName() + " is ready to do it's business.");
+                System.out.println(Thread.currentThread().getName() + " is ready to do it's business. latch=" + latch.getCount());
                 try {
                     latch.await(); //  需要等待计数器的值变为0，后面的代码才会执行，如果一个线程将当前线程给interrupt了，那它也是可以继续执行的
                 } catch (InterruptedException e) {
-                    System.out.println(Thread.currentThread().getName() + " has been interrupted.");
+                    System.out.println(Thread.currentThread().getName() + " has been interrupted. latch=" + latch.getCount());
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName() + " has finished it's business.");
+                for (int i = 0; i < 3; i++) {
+                    System.out.println(Thread.currentThread().getName() + " continue does it's business work. latch=" + latch.getCount());
+                }
+                System.out.println(Thread.currentThread().getName() + " has finished it's business. latch=" + latch.getCount());
             }
         }
     }
@@ -64,6 +67,7 @@ public class UseCountDownLatch {
         System.out.println(Thread.currentThread().getName() + " does it's work.");
         */
 
+        /*
         // 先启动initThread
         InitThread initThread = new InitThread();
         initThread.start();
@@ -77,6 +81,36 @@ public class UseCountDownLatch {
 
         //  会唤醒businessThread之前阻塞的方法，而代码并不会从头开始执行，仅仅是唤醒
         businessThread.interrupt();
+        */
 
+        //  单独的初始化线程
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Thread.currentThread().setName("singleInitThread");
+                System.out.println(Thread.currentThread().getName() + " is ready to do it's step one. latch=" + latch.getCount());
+                SleepTools.sleepForMilliseconds(1);
+                System.out.println(Thread.currentThread().getName() + " has finished it's step one. latch=" + latch.getCount());
+                latch.countDown(); // 计数器扣减1
+                System.out.println(Thread.currentThread().getName() + " is ready to do it's step two. latch=" + latch.getCount());
+                SleepTools.sleepForMilliseconds(1);
+                System.out.println(Thread.currentThread().getName() + " has finished it's step two. latch=" + latch);
+                latch.countDown(); // 计数器扣减1
+            }
+        }).start();
+
+        (new BusinessThread()).start();
+
+        for (int i = 0; i < 4; i++) {
+            (new InitThread()).start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Thread.currentThread().getName() + " continue does it's work. latch=" + latch.getCount());
     }
 }
